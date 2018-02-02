4b6214cf35bceba7b5d70df07165b6d1098bb324
DejanJ90 <dejan.jurisic90@gmail.com>
2018-02-02 15:56:23 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var number = 25 
+if(typeof num === "number") {
+    if(num > 9 && nm<100) {
+
+        var secondNumber = num % 10
+        var firstNumber = (num - secondNumber) /10
+        var result = secondNumber * 10 + firstNumber
+        console.log (result)
+    } else {
+        console.log ("Error")
+    } else {
